3685b263d8e04c5dddb23ac293f1097bf96b722c
Мохаммад Дидаев <mohammaddidaev@MacBook-Air-Mohammad.local>
2023-09-21 22:58:48 +0300

    add styles and page theme with custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { Route, Routes, Link } from 'react-router-dom';
-import './index.scss'
+import './styles/index.scss'
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { Suspense } from 'react';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+
+    const { theme, toggleTheme } = useTheme()
+
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>toggle</button>
             <Link to='/'>Главная</Link>
             <Link to='/about'>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
